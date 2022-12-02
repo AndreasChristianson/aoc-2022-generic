f0fbf8e4a18a066066deb2b1590b1df94b1304b6
Andreas Christianson <achristianson@pessimistic-it.com>
2022-12-02 17:29:54 -0600

    some boilerplate

--- day3/src/main/kotlin/com/pessimistic/Hello.kt
+++ day3/src/main/kotlin/com/pessimistic/Hello.kt
@@ -1,6 +1,15 @@
 package com.pessimistic
 
+import java.io.File
+
 fun main(args: Array<String>) {
+  val pattern = Regex("(.*)")
     println("Hello, World")
+    val text = File("test.txt").readText()
+      .lines()
+      .filter { it.length>0 }
+      .map { pattern.find(it) }
+      .map{ it!!.groupValues.slice(1..1)}
+    println(text)
 }
 
