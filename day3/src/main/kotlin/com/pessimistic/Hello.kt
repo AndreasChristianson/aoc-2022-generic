4772170c15fc03f68931a22b6a19bf4583505bf9
Andreas Christianson <achristianson@pessimistic-it.com>
2022-12-02 23:51:09 -0600

    day3

--- day3/src/main/kotlin/com/pessimistic/Hello.kt
+++ day3/src/main/kotlin/com/pessimistic/Hello.kt
@@ -3,13 +3,37 @@ package com.pessimistic
 import java.io.File
 
 fun main(args: Array<String>) {
-  val pattern = Regex("(.*)")
-    println("Hello, World")
-    val text = File("test.txt").readText()
-      .lines()
-      .filter { it.length>0 }
-      .map { pattern.find(it) }
-      .map{ it!!.groupValues.slice(1..1)}
-    println(text)
-}
+  fun findCommonChar(list: List<String>): Char {
+    return list.drop(1)
+        .fold(list.first().toSet(), { acc, string -> acc.intersect(string.toList()) })
+        .first()
+  }
+  fun toPriority(item: Char): Int {
+    if ((65..90).contains(item.code)) {
+      return item.code - 65 + 27
+    } else {
+      return item.code - 97 + 1
+    }
+  }
+  val part1 =
+      File("input.txt")
+          .readText()
+          .lines()
+          .filter { it.length > 0 }
+          .map { it.chunked(it.length / 2) }
+          .map { findCommonChar(it) }
+          .map { toPriority(it) }
+          .sum()
+  println(part1)
 
+  val part2 =
+      File("input.txt")
+          .readText()
+          .lines()
+          .filter { it.length > 0 }
+          .chunked(3)
+          .map { findCommonChar(it) }
+          .map { toPriority(it) }
+          .sum()
+  println(part2)
+}
