7d980aa580ec67a8f9a742e628c37a8c22283c59
Andreas Christianson <achristianson@pessimistic-it.com>
2022-12-03 23:33:25 -0600

    day4

--- day4/aoc.py
+++ day4/aoc.py
@@ -1,4 +1,33 @@
-with open("test.txt") as file:
+import re
+
+
+def fullyOverlaps(first, second):
+    if(min(first) <= min(second) and max(first) >= max(second)):
+        return True
+    return False
+
+
+def anyOverlap(first, second):
+    if(min(first) <= max(second) and min(first) >= min(second)):
+        return True
+    return False
+
+
+pattern = '(\d+)-(\d+),(\d+)-(\d+)'
+count = 0
+count2 = 0
+with open("input.txt") as file:
     lines = [line.rstrip() for line in file]
     for line in lines:
-        print(line)
+        matches = re.finditer(pattern, line)
+        for match in matches:
+            left = (int(match.group(1)), int(match.group(2)))
+            right = (int(match.group(3)), int(match.group(4)))
+            if(fullyOverlaps(left, right) or fullyOverlaps(right, left)):
+                count = count+1
+            if(anyOverlap(left, right) or anyOverlap(right, left)):
+                count2 = count2+1
+
+
+print(count)
+print(count2)
