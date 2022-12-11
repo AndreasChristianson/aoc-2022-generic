1cb101194c14f66a1d1428bf22c78b198a034eec
Andreas Christianson <achristianson@pessimistic-it.com>
2022-12-11 13:57:43 -0600

    formatting

--- day10/main.ts
+++ day10/main.ts
@@ -4,48 +4,43 @@ if (import.meta.main) {
     .split("\n")
     .filter((line) => line.length > 0);
   let value = 1;
-  let sum =0 ;
-  let param = 0
-  let index = 0
-  let crt = ""
+  let sum = 0;
+  let param = 0;
+  let index = 0;
+  let crt = "";
   for (let cycle = 1; cycle < 240; cycle++) {
-    if((cycle-20)%40 == 0){
-      sum+=cycle*value
+    if ((cycle - 20) % 40 == 0) {
+      sum += cycle * value;
     }
-    const pos = cycle%40-1
-    if(Math.abs(value-(pos))<2){
-      crt+="#"
-    }else{
-      crt+=" "
-    }
-  
-    if(cycle%40 == 0){
-      console.log(`line end`);
-
-      crt+="\n"
-
+    const pos = (cycle % 40) - 1;
+    if (Math.abs(value - pos) < 2) {
+      crt += "#";
+    } else {
+      crt += " ";
     }
 
+    if (cycle % 40 == 0) {
+      crt += "\n";
+    }
 
-
-    if(param != 0){
-      value+=param
-      param = 0
-    }else if (index < lines.length) {
+    if (param != 0) {
+      value += param;
+      param = 0;
+    } else if (index < lines.length) {
       const line = lines[index];
-      index++
+      index++;
       const splitLine = line.split(" ");
       switch (splitLine[0]) {
         case "noop":
           break;
         case "addx":
-          param =  parseInt(splitLine[1])
+          param = parseInt(splitLine[1]);
           break;
         default:
           throw Error("Uh, guys..");
       }
     }
   }
-  console.log(sum)
-  console.log(crt)
+  console.log(sum);
+  console.log(crt);
 }
