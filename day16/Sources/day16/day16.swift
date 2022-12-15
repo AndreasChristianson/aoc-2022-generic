00b961530507dd6ae0f8a23a47adc197a49d47f7
Andreas Christianson <achristianson@pessimistic-it.com>
2022-12-15 17:27:43 -0600

    swift tonight

---
+++ day16/Sources/day16/day16.swift
@@ -0,0 +1,8 @@
+@main
+public struct day16 {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(day16().text)
+    }
+}
---
+++ day16/Tests/day16Tests/day16Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import day16
+
+final class day16Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(day16().text, "Hello, World!")
+    }
+}
